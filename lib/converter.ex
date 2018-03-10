367cea52173ce924d8f24f26fac5e3797c44098e
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-03-09 16:28:42 -0800

    made converter functions a bit more explicit

--- lib/converter.ex
+++ lib/converter.ex
@@ -1,10 +1,10 @@
 defmodule Converter do
 
-  def to_nearest_tenth(val) do
+  def to_nearest_tenth(val) when is_integer(val) or is_float(val) do
     round_to(val, 1)
   end
 
-  def to_km(val) do
+  def to_km(val) when is_integer(val) or is_float(val) do
     val / 1000
   end
 
@@ -18,21 +18,24 @@ defmodule Converter do
 
   def round_up(val) when is_float(val), do: trunc(val)
 
-  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
-  def to_light_seconds({unit, val}, precision: precision) do
-    lights_seonds = case unit do
-      :miles -> from_miles(val)
-      :meters -> from_meters(val)
-      :feet -> from_feet(val)
-      :inches -> from_inches(val)
-    end
-    lights_seonds |> round_to(precision)
+  def to_light_seconds({:miles, miles} = val, precision: precision) when is_integer(miles) or is_float(miles) do
+    (miles * 5.36819e-6) |> round_to(precision)
   end
 
-  defp from_miles(val), do: val * 5.36819e-6
-  defp from_meters(val), do: val * 3.335638620368e-9
-  defp from_feet(val), do: val * 1.016702651488166404e-9
-  defp from_inches(val), do: val * 8.472522095734715723e-11
-  defp round_to(val, precision \\ 5), do: Float.round(val, precision)
+  def to_light_seconds({:meters, meters} = val, precision: precision) when is_integer(meters) or is_float(meters)  do
+    (meters * 3.335638620368e-9) |> round_to(precision)
+  end
+
+  def to_light_seconds({:feet, feet} = val, precision: precision) when is_integer(feet) or is_float(feet)  do
+    (feet * 1.016702651488166404e-9) |> round_to(precision)
+  end
+
+  def to_light_seconds({:inches, inches} = val, precision: precision) when is_integer(inches) or is_float(inches)  do
+    (inches * 8.472522095734715723e-11) |> round_to(precision)
+  end
+
+  defp round_to(val, precision) when is_float(val) do
+    Float.round(val, precision)
+  end
 
 end
